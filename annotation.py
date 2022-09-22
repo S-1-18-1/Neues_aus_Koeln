3b1b5f6a58ea6942c708da2d427a98d73cf6e17e
Sara Saleh <ssaleh2@smail.uni-koeln.de>
2022-09-22 15:03:48 +0200

    added annotation.py

---
+++ annotation.py
@@ -0,0 +1,90 @@
+from helper.random_story import get_random_stories
+from lxml import etree as ET
+import os
+
+parser = ET.XMLParser(remove_blank_text=True)
+
+
+def get_count_of_existing_annotations(input_path):
+    """counts the already existing annotations
+
+    :param input_path: path to the corpus folder
+    :type input_path: str
+    :return: count of existing annotations
+    :rtype: int
+    """
+    count=0
+    for root, dir, files in os.walk(input_path):
+        for file_name in files:
+            full_path = os.path.join(input_path, file_name)
+            tree = ET.parse(full_path, parser)
+            count += tree.xpath("count(//exceptionalNormal[text()])")
+    return count
+
+def console_print(text):
+    """manages the console out- and input
+
+    :param text: Text to be displayed
+    :type text: str
+    :return: user input
+    :rtype: str
+    """
+    print("%s \nExceptional Normal? [y/n/exit] " %text)
+    user_answer = input().lower()
+    if user_answer!="y" and user_answer!="n" and user_answer!="exit": 
+        print("%s is not a valid value. Please choose between 'y' or 'n' or 'exit'" %user_answer)
+        console_print(text)
+    return user_answer
+
+
+def annotate(input_path, story, log):
+    """calls functions to annotate the story in the console and saves the annotation in the xml file
+
+    :param input_path: path to the corpus
+    :type input_path: str
+    :param story: list with [lxml.etree._Element, str]
+    :type story: list
+    :param log: path for the logfile
+    :type log: str
+    :return: False, if the user inputs "exit"
+    :rtype: bool/none
+    """
+
+    # get the original xml tree
+    full_path = os.path.join(input_path, story[1])
+    tree = ET.parse(full_path, parser)
+
+    user_answer = console_print(story[0].find(".//text").text)
+    if user_answer == "exit":
+        # break condition
+        return False
+    else: 
+        number = story[0].find(".//number").text
+        print(number)
+
+        # write the annotation result in the original xml
+        tree.xpath('.//story[number="%s"]' %number)[0].find("./exceptionalNormal").text = user_answer
+        tree.write("%s" %full_path, pretty_print=True, encoding='utf-8')  
+
+        #log entry   
+        with open(log, "a") as f:
+            f.write("Added annotation for story number %s in %s \n" %(number, story[1]))   
+
+    
+if __name__ == '__main__':
+
+    # paths
+    input_path = "corpus"
+    log = "output/annotation_log.txt"
+
+    #
+    desired_annotation_count = 200
+    existing_annotation_count = get_count_of_existing_annotations(input_path)
+    count= int(desired_annotation_count - existing_annotation_count)
+
+    # get as many random stories as needed for the remaining files to annotate
+    stories = get_random_stories(input_path, count, "return", already_annotated=False)
+    for story in stories:
+        print ("Remaining annotations: %s/%s" %(count, desired_annotation_count))
+        if annotate(input_path, story, log) == False:
+            break
--- helper/random_story.py
+++ helper/random_story.py
@@ -2,25 +2,51 @@ from random import randint
 from lxml import etree as ET
 import os
 
-path="corpus"
-output = "output/randomstories.txt"
-stories = []
+def get_random_stories(input, count, mode, output ="", already_annotated = True):
+    """chooses random stories
 
-parser = ET.XMLParser(remove_blank_text=True)
+    :param input: path to the corpus
+    :type input: str
+    :param count: count of random stories
+    :type count: int
+    :param mode: "print" for printing in output file, "return" for returning the list of random stories
+    :type mode: str
+    :param output: only needed for mode "print", path to the output file
+    :type output: str
+    :param already_annotated: True, if already annotated stories should be collected too
+    :type output: bool
+    :return: the list with random stories
+    :rtype: list/none
+    """
 
-for root, dir, files in os.walk(path):
-    for file_name in files:
-        full_path = os.path.join(path, file_name)
-        tree = ET.parse(full_path, parser)
-        for story in tree.findall(".//text"):
-            stories.append([story.text, file_name])
+    stories = []
+    rand_stories = []
 
+    parser = ET.XMLParser(remove_blank_text=True)
 
-with open(output, "w") as f:
-    i=0
-    while i < 100:
+    for root, dir, files in os.walk(input):
+        for file_name in files:
+            full_path = os.path.join(input, file_name)
+            tree = ET.parse(full_path, parser)
+            # select only stories with none empty texts
+            if already_annotated == True:
+                for story in tree.xpath(".//story[not(. = '')]"):
+                    stories.append([story, file_name])
+            elif already_annotated == False:
+                for story in tree.xpath(".//story[not(. = '') and //exceptionalNormal[not(text())]]"):
+                    stories.append([story, file_name])
+    i = 0 
+    while i < count:
         get_story = randint(0,len(stories))
-        f.write(str(stories[get_story])+"\n")
+        rand_stories.append(stories[get_story])
         i+=1
+    if mode == "print":
+        with open(output, "w") as f:
+            for story in rand_stories:
+                f.write( "%s: %s \n" %(story[1], story[0].find(".//text").text))
+    elif mode =="return":
+        return rand_stories
 
-print(len(stories))
+
+if __name__ == '__main__':
+    get_random_stories("corpus", 100, mode="print", output="output/randomstories.txt", already_annotated = True)
